acc10986c9c97c88e429b915efdfe7b224e35844
robioat <roates@gmail.com>
2022-04-05 13:02:02 +0100

    4-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,39 @@
-const sumAll = function() {
+let isNumber = function(input){
+    if(typeof(input)=="number"){
+        return true;
+    }
+}
 
+let isPositive = function(input){
+    if(input>0){
+        return true;
+    }
+}
+
+let getSmallest = function(i,j){
+    if(i<j){
+        return i;
+    }
+    return j;
+}
+
+let getLargest = function(i,j){
+    if(i>j){
+        return i;
+    }
+    return j;
+}
+
+let sumAll = function(i,j) {
+    if(isNumber(i) && isNumber(j) && isPositive(i) && isPositive(j)){
+        for(n=getSmallest(i,j),total=0; n<=getLargest(i,j); n++){
+            total+=n;
+        }
+        return total;
+    }
+    else{
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+let round1Dp = function(num){
+  return Math.round(num*10)/10;
+}
 
+const ftoc = function(temp) {
+  return round1Dp(((temp-32)*5)/9);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return round1Dp((temp/5)*9+32);
 };
 
 // Do not edit below this line
